a8dc4205aa2d0631284546838e3f1a8e513fd969
Anton P Robul <mail@anton-slim.com>
2025-01-14 13:12:22 +0300

    Добавлен health-check (/api/utils/health-check)

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import mushrooms, baskets
+from app.api.routes import mushrooms, baskets, utils
 
 api_router = APIRouter()
 api_router.include_router(mushrooms.router)
 api_router.include_router(baskets.router)
+api_router.include_router(utils.router)
 
